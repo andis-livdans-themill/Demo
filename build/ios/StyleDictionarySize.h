48db284d06a96041dcdd32367687a2651f6b42e5
alivdans-artidis <andis.livdans@themill.io>
2025-03-02 13:53:31 +0200

    testset

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Sun, 02 Mar 2025 11:47:02 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
